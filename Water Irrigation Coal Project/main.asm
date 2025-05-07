1b9f95edb96ccbe5e711b6de1726ab1437f26656
Abdul Rehman <abdulrehmansafdar2928@gmail.com>
2025-05-07 17:00:56 +0500

    initial

---
+++ Water Irrigation Coal Project/main.asm
@@ -0,0 +1,52 @@
+.include "m328pdef.inc"
+.include "delay_Macro.inc"
+.include "1602_LCD_Macros.inc"
+.cseg
+.org 0x0000
+LCD_init ; initilize the 16x2 LCD
+LCD_backlight_OFF
+delay 500
+LCD_backlight_ON
+loop:
+; Display a string on LCD
+LDI ZL, LOW (2 * hello_string)
+LDI ZH, HIGH (2 * hello_string)
+LDI R20, string_len
+LCD_send_a_string
+delay 1000
+LCD_send_a_command 0x01 ; clear the LCD
+; Display an integer on LCD
+LDI r16, 123
+LCD_send_a_register r16
+delay 1000
+LCD_send_a_command 0x01 ; clear the LCD
+; Display character on LCD
+; Sending Hello World to LCD character-by-character
+LCD_send_a_character 0x48 ; 'H'
+LCD_send_a_character 0x45 ; 'E'
+LCD_send_a_character 0x4C ; 'L'
+LCD_send_a_character 0x4C ; 'L'
+LCD_send_a_character 0x4F ; 'O'
+LCD_send_a_character 0x20 ; ' ' (space)
+LCD_send_a_character 0x57 ; 'W'
+LCD_send_a_character 0x4F ; 'O'
+LCD_send_a_character 0x52 ; 'R'
+LCD_send_a_character 0x4C ; 'L'
+LCD_send_a_character 0x44 ; 'D'
+LCD_send_a_character 0x21 ; '!'
+LCD_send_a_command 0xC0 ; move curser to next line
+LCD_send_a_character 0x43 ; 'C'
+LCD_send_a_character 0x4F ; 'O'
+LCD_send_a_character 0x41 ; 'A'
+LCD_send_a_character 0x4C ; 'L'
+LCD_send_a_command 0x14 ; move curser one step forward (another way to add space)
+LCD_send_a_character 0x4C ; 'L'
+LCD_send_a_character 0x41 ; 'A'
+LCD_send_a_character 0x42 ; 'B'
+delay 1000
+LCD_send_a_command 0x01 ; clear the LCD
+rjmp loop
+; it is recommanded to define the strings at the end of the code segment
+; The length of the string must be even number of bytes
+hello_string: .db "Tehseen.",0
+len: .equ string_len = (2 * (len - hello_string)) - 1
